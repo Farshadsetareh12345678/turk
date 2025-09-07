b01b22e3a058fc602c66c6ff98e4b949cfce03fe
TURKDAROO <farshadsetare304@gmail.com>
2025-09-07 22:36:01 +0330

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// اگر روی GitHub Pages دیپلوی می‌کنی، base را به صورت "/<repo-name>/" تنظیم کن
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // برای Vercel همین بمونه. اگر GitHub Pages: base: '/crypto-assistant-pro/'
-})
